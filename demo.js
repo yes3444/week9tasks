1ed683f6cf242b05ef325fb03286c2a82805cb6a
yes3444 <nehmataslam@gmail.com>
2026-03-13 18:16:08 +0000

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,35 @@
 
+{
+    document.addEventListener("DOMContentLoaded", function() {
+  
+    fetch("https://huayan.pythonanywhere.com/animals")
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById("animalList");
+            data.forEach(fruit => {
+                list.innerHTML += "<li>" + fruit + "</li>";
+            });
+        });
+});
+}
+
+
+function getStudentInfo(){
+    const inputName = document.getElementById("inputName").value;
+  
+    fetch("https://huayan.pythonanywhere.com/studentInfo",{
+        method: "POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({name:inputName})
+    })
+    .then(response => response.json())
+    .then(data =>{
+        document.getElementById("photo").src = data.photo;
+            document.getElementById("name").textContent = data.name;
+            document.getElementById("age").textContent = data.age;
+            document.getElementById("course").textContent = data.course;
+            document.getElementById("level").textContent = data.level;
+            document.getElementById("department").textContent = data.department;
+    });
+}
+
